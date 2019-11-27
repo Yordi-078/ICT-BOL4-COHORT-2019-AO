8280b75cd75817644fe70ed039f3be961cf417bc
Yordi <99056993@mydavinci.nl>
2019-11-27 13:31:43 +0100

    Adventure game

---
+++ AO-Opdrachten/Blok 2/week 1/js/adventure.js
@@ -0,0 +1,169 @@
+
+    
+var description = document.getElementById("description");
+var image = document.getElementById("image");
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var inventoryItem = document.getElementById("inventoryItem");
+
+var key = false;
+
+function startGame(){
+  inventoryItem.style.display = 'none';
+  button1.innerHTML = "Start";
+  button1.setAttribute('onclick','begin()');
+  button2.style.display = 'none';
+  button3.style.display = 'none';
+  description.innerHTML = "  <h2> Maak je keuze!! </h2> <br> <br> Je bent gepakt bij een  bankoverval. <br>  Je hebt 20 jaar celstraf gekregen. <br> Jij gaat uit de gevangenis ontsnappen!!";
+}
+
+function begin(){
+description.innerHTML = "<h2> Maak je keuze!! </h2> <br> <br> Je kan op de pauze wachten en de omgeving verkennen. <br> Of je wordt helemaal boos en sloopt alles in je cel!";
+image.src ='img/tralies.jpg'
+button1.innerHTML = "Wachten op de pauze";
+button1.setAttribute('onclick','pauze()');
+button2.style.display = 'inline-block';
+ button2.innerHTML = "Boos worden";
+ button2.setAttribute('onclick','boosWorden()');
+}
+
+function boosWorden(){
+  description.innerHTML = " <h2> (fout!!) </h2> <br> <br> Je werd boos en werd uit je cel gehaald en meegenomen en in de isoleercel gestopt!!";
+  button1.innerHTML = "Ga terug";
+button1.setAttribute('onclick','begin()');
+button2.style.display = 'none';
+}
+
+function pauze(){
+  image.src ='img/vechten.jpg'
+  description.innerHTML = "<h2> Maak je keuze!! </h2> <br> <br> Het is pauze. Je loopt met de rest naar de luchtplaats. <br> Op de buitenplaats komt er een groepje naar je toe. <br> De leider geeft je een stoot en pakt je vast en zegt: laat zien dat je een man bent en doe wat. <br> <br> Je slaat hem terug!";
+  button1.innerHTML = "Vechten";
+  button1.setAttribute('onclick','vechten()');
+  button2.style.display = 'none';
+}
+
+function vechten(){
+  image.src ='img/afmaken.jpg'
+  description.innerHTML = "<h2> Maak je keuze!! </h2> <br> <br> Je pakt hem bij zen keel en hij begint je te slaan. <h4> Je geeft hem een goede linker hij wankelt!</h4>  <p> Je kan het afmakenm. </p> <br>"; 
+  button1.innerHTML = "Geef hem een goede rechter!";
+  button1.setAttribute('onclick','afmaken()');
+  button2.style.display = 'inline-block';
+ button2.innerHTML = "Geef hem een goede linker!";
+ button2.setAttribute('onclick','afmaken()');
+ button3.style.display = 'inline-block';
+ button3.innerHTML = "Geef hem een hoge schop!";
+ button3.setAttribute('onclick','afmaken()');
+}
+
+function afmaken(){
+  image.src ='img/groep.jpg'
+  description.innerHTML = "<h2> Maak je keuze!! </h2> <br> <br> De politie grijpt in!! <br> Jullie worden beiden voor 10 dagen in de isoleercel gestopt. <br> na 10 dagen ben je er uit en mag je weer pauze houden. <br> Dezelfde groep komt weer naar je toe maar dit keer helpt een andere groep jouw en laten de andere groep weg gaan. <br> Bart geeft je een hand en zegt: kom met ons mee. ";
+  button1.innerHTML = "Ga mee";
+  button1.setAttribute('onclick','meeGaan()');
+  button2.style.display = 'none';
+  button3.style.display = 'none';
+}
+
+function meeGaan(){
+  image.src ='img/meegaan.jpg'
+  description.innerHTML = "<h2> Maak je keuze!! </h2> <br> <br> Bart neemt je apart en zegt: ik zie je hele tijd overal kijken en heb een gevoel dat je hier ook niet lang wilt blijven. <br> Doe je mee met een ontsnapping?";
+  button1.innerHTML = "Jaa";
+  button1.setAttribute('onclick','jaa()');
+  button2.style.display = 'inline-block';
+  button2.innerHTML = "Nee laat me met rust";
+  button2.setAttribute('onclick','nee()');
+}
+
+function nee(){
+  button1.innerHTML = "Ga terug";
+  button1.setAttribute('onclick','meeGaan()');
+  description.innerHTML = "<h2> Fout!! </h2> <br> <br> In je eentje kan je niet ontsnappen!!";
+  button2.style.display = 'none';
+}
+
+function jaa(){
+  image.src ='img/stalendeur.jpg'
+  description.innerHTML = "<h2> Maak je keuze!! </h2> <br> <br> Jullie lopen rustig zonder opgemerkt te worden naar de deur waar alle spullen liggen. <br> Als jullie om de hoek zijn kunnen jullie niet meer gezien worden omdat het om de hoek is.";
+  button2.style.display = 'none';
+  button1.innerHTML = "Maak de deur open";{
+  if(key == true){
+  button1.setAttribute('onclick','openDeur()');
+  } else{
+  button1.setAttribute('onclick',' keyNodig()');
+}
+  }
+
+}
+
+function keyNodig(){
+  image.src ='img/sleutelbos.jpg'
+  description.innerHTML = "<h2> Fout!! </h2> <br> <br> Jullie hebben een sleutel nodig <br> ";
+  button1.innerHTML = "Haal de sleutel";
+  button1.setAttribute('onclick','vraag()');
+}
+
+function vraag(){
+  image.src ='img/politie.jpg'
+  description.innerHTML = "<h2> Maak je keuze!! </h2> <br> <br> Bart legt je het plan uit. <br> Voordat we kunnen ontsnappen hebben we een sleutel nodig naar de kamer waar alle spullen liggen. <br> De sleutel ligt naast de bewaker als jij hem afleid voor ons dan pakken we de sleutel. <br> <br> Maak een keuze hoe je hem afleid!";
+  button1.innerHTML = "Wanneer moet ik naar de cel?";
+  button1.setAttribute('onclick','fouteVraag()');
+  button2.style.display = 'inline-block';
+  button2.innerHTML = "Ga op de grond liggen en doe of je pijn hebt";
+  button2.setAttribute('onclick','nepPijn()');
+
+}
+
+ function fouteVraag(){
+   description.innerHTML = "<h2> Fout!! </h2> <br> <br> Het is een te korte vraag de bewaker gaf antwoord en ging weer terug.";
+   button1.innerHTML = "Ga terug";
+   button1.setAttribute('onclick','vraag()');
+   button2.style.display = 'none';
+  
+ }
+
+ function nepPijn(){
+ key = true
+  jaa();
+   description.innerHTML = "<h2> Maak je keuze!! </h2> <br> <br> Ze hebben de sleutel te pakken. <br> Ga weer rustig terug naar de kamer waar alle spullen liggen. ";
+  button1.innerHTML = "Ga erheen";
+ button1.setAttribute('onclick',' jaa()');
+   button2.style.display = 'none';
+ }
+
+ function openDeur(){
+  button1.innerHTML = "Haal de sleutel";
+  button1.setAttribute('onclick','vraag()');
+  button2.innerHTML = "Haal de sleutel";
+  button2.setAttribute('onclick','vraag()');
+ }
+
+
+
+
+
+
+startGame();
+
+
+// inventoryItem.style.display = 'none';
+// button1.innerHTML = "help";
+// button1.setAttribute('onclick','level2()');
+// button2.style.display = 'none';
+// button3.style.display = 'none';
+// }
+
+// function level2(){
+// image.src ='img/tralies.jpg'
+// button1.innerHTML = "slaan";
+// button1.setAttribute('onclick','getShovel()');
+// button2.style.display = 'inline-block';
+// button2.innerHTML = "ga terug";
+// button3.style.display = 'inline-block';
+// button3.innerHTML = "verder";
+// }
+
+// function getShovel(){
+//   shovel = treu;
+//   level2();
+// }
